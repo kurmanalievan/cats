140c4dec88e5f6fae870a69dc62788a7dbfb91e1
nursezim <kurmanalieva.nurs@gmail.com>
2024-03-26 16:07:38 +0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { useState, useEffect } from 'react'
+import axios from 'axios';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import './App.css'
 
 function App() {
+
+  const [imgUrl, setImgUrl] = useState()
+  
+  const getData = async () => {
+    const response = await axios.get("https://api.thecatapi.com/v1/images/search")
+    const url = response.data[0].url
+    setImgUrl(url)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={imgUrl} />
+      <Card.Body>
+        <Card.Title>Kitty</Card.Title>
+        <Card.Text>
+          If you are a cat lover, enjoy cat images by clicking the button below!
+        </Card.Text>
+        <Button variant="primary" onClick={getData}>Cat please!</Button>
+      </Card.Body>
+    </Card>
     </div>
   );
 }
